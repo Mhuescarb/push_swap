4eab2a7f8cc3778f60732bf0aa288b9bf6534c35
mhuescar <mhuescar@student.42malaga.com>
2025-03-07 14:34:07 +0100

    7 marzo

---
+++ makefile
@@ -0,0 +1,36 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mhuescar <mhuescar@student.42malaga.com    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/03/07 13:04:29 by mhuescar          #+#    #+#              #
+#    Updated: 2025/03/07 13:25:38 by mhuescar         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = push_swap.a
+
+CC = CC
+CFLAGS = -Wall -Werror -Wextra
+
+SRCS =  
+
+
+
+OBJS= $(SRCS:.c=.o)
+
+$(NAME): $(OBJS)
+		 $(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+
+all: $(NAME)
+
+clean:
+	rm -f $(OBJS)
+	
+fclean: clean
+		rm -f $(NAME)
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ push_swap.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhuescar <mhuescar@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 12:37:06 by mhuescar          #+#    #+#             */
+/*   Updated: 2025/03/07 13:50:41 by mhuescar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <limits.h>
+
+
+typedef struct s_stack
+{
+	int		value; // elemento a ordenar
+	int		index; // indice en la lista de los elementos a ordenar
+	int		position; // posicion en la pila
+	int		target_position;// posicion objetivo en A de los elementos de B
+	int		a_cost;// coste de mover el elemento al top de la pila A
+	int		b_cost;// coste de mover el elemento al top de la pila B
+	int		all_cost; // ?
+	struct s_stack	*next;//puntero al siguiente elemento de la lista
+}	t_stack;
+
+
+#endif
